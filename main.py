52b6814562de078c587244c6f3995ef4826a0800
Tejas Joshi <tejasjoshi4598@gmail.com>
2026-03-14 19:44:32 +0530

    first commit

---
+++ main.py
@@ -0,0 +1,13 @@
+import qrcode
+from tkinter import *
+from tkinter import ttk
+
+# A simple graphical interface intended to make a QR code out of any given string, complete with advanced customisation options.
+
+def makeQR(toConvert, outfile):
+    img = qrcode.make(toConvert)
+    type(img)
+    img.save(outFile)
+
+
+
