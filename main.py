00d4b974b1fc875f05f852ab4ffac11048230a9e
Tejas Joshi <tejasjoshi4598@gmail.com>
2026-03-14 19:52:20 +0530

    Changes to be committed: 	modified:   README.md 	modified:   main.py

--- main.py
+++ main.py
@@ -4,10 +4,10 @@ from tkinter import ttk
 
 # A simple graphical interface intended to make a QR code out of any given string, complete with advanced customisation options.
 
-def makeQR(toConvert, outfile):
+def makeQR(toConvert, outFile):
     img = qrcode.make(toConvert)
     type(img)
     img.save(outFile)
-                    
 
+makeQR(input("enter: "), "outfile.png")
 
